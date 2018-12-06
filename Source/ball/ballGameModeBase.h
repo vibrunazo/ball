8b776830bb08f93f45801ea12cd98eec9c4182e7
Vandré Brunazo <vibrunazo@gmail.com>
2018-12-06 10:20:35 -0200

    'ini'

---
+++ Source/ball/ballGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ballGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BALL_API AballGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
